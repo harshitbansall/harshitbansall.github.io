1e7ccca278574ce9bab0e804252109aba302ab35
harshitbansall <harshitbansal587@gmail.com>
2022-03-02 19:18:21 +0530

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-25dd20117014e5b18383.js"
+    "url": "webpack-runtime-6d3060b2af3953981a55.js"
   },
   {
     "url": "framework-cfc39a90b11063613693.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "0776037c8f7c33f7a59af4f6b7108aff"
+    "revision": "dd0842e502ca64378c701dbfc261e9dd"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-b5bc8a925215f7166f76.js"
